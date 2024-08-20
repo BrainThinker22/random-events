c25d2259f893836e915d160bc21b80e15ac80391
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-20 15:26:37 +0200

    Add files via upload

---
+++ random-generator.cpp
@@ -0,0 +1,90 @@
+/*
+
+        // RANDOM COMPETITION FINISH CODE
+
+    #include <iostream>
+    #include <ctime>
+
+    int main()
+{
+    srand(time(0));
+    int randomEvent = rand() % 9 + 1;
+
+    switch (randomEvent) {
+
+    case 1:
+        std::cout << "You will win!" << '\n';
+        break;
+    case 2:
+        std::cout << "You will finish second!" << '\n';
+        break;
+    case 3:
+        std::cout << "You will finish third!" << '\n';
+        break;
+    case 4:
+        std::cout << "You will finish fourth!" << '\n';
+        break;
+    case 5:
+        std::cout << "You will finish fifth!" << '\n';
+        break;
+    case 6:
+        std::cout << "You will finish sixth!" << '\n';
+        break;
+    case 7:
+        std::cout << "You will finish seventh!" << '\n';
+        break;
+    case 8:
+        std::cout << "You will finish eighth!" << '\n';
+        break;
+    case 9:
+        std::cout << "You will finish ninth!" << '\n';
+        break;
+    case 10:
+        std::cout << "You will finish last!" << '\n';
+        break;
+    }
+
+    return 0;
+
+}
+
+*/
+
+/*
+
+        // RANDOM CHOICE WHERE TO EAT CODE
+
+    #include <iostream>
+    #include <ctime>
+
+    int main() {
+
+    srand(time(0));
+    int randomChoice = rand() % 6 + 1;
+
+    switch (randomChoice) {
+    case 1:
+        std::cout << "I will eat at home!" << '\n';
+            break;
+    case 2:
+        std::cout << "I will eat at a restaurant!" << '\n';
+        break;
+    case 3:
+        std::cout << "I will eat at McDonald's!" << '\n';
+        break;
+    case 4:
+        std::cout << "I will go to the shop and buy some food!" << '\n';
+        break;
+    case 5:
+        std::cout << "I will eat at a fast food restaurant!" << '\n';
+        break;
+    case 6:
+        std::cout << "I won't eat today anymore!" << '\n';
+        break;
+    }
+
+    return 0;
+
+}
+
+*/
